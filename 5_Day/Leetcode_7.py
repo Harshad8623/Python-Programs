112d34953505712558d57fa2756cd5364b2a5257
harshad8623 <dhuppeh@gmail.com>
2026-05-10 09:52:41 +0530

    Auto update

--- 5_Day/Leetcode_7.py
+++ 5_Day/Leetcode_7.py
@@ -10,7 +10,6 @@
 class Solution(object):
     def isValid(self, s):
         stack = []
-        
         mapping = {')': '(', '}': '{', ']': '['}
         for char in s:
             if char in mapping:
